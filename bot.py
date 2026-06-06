e4278417539a525d851a6fe3dff16c5c65fc3a63
j1beast <javism26@googlemail.com>
2026-06-06 19:24:59 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -229,7 +229,7 @@ def guardar_prediccion(jugador_a, franq_a, jugador_b, franq_b, analisis, betsson
         under_bs = betsson.get("cuota_under")
     es_valor = 1 if betsson and betsson.get("es_valor") else 0
     
-   c.execute('''INSERT INTO predicciones
+    c.execute('''INSERT INTO predicciones
        (jugador_a, jugador_b, franq_a, franq_b, ganador_predicho, cuota_ganador,
         linea_total, cuota_over, cuota_under, prediccion_ou, fecha_prediccion, procesado,
         prob_h2h, prob_equipo, prob_h2h_eq, prob_forma, prob_h2h_rec,
