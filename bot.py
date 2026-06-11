692fb9798928e5d85772a162f0ceb5f0a8b3bb87
j1beast <javism26@googlemail.com>
2026-06-11 16:34:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -237,7 +237,7 @@ def guardar_prediccion(jugador_a, franq_a, jugador_b, franq_b, analisis, betsson
     c.execute('''SELECT id, cuota_betsson_a FROM predicciones 
                  WHERE ((jugador_a=? AND jugador_b=?) OR (jugador_a=? AND jugador_b=?))
                  AND fecha_prediccion LIKE ?
-                 AND procesado IN (0, 1)''',
+                 AND procesado = 0''',
               (jugador_a, jugador_b, jugador_b, jugador_a, f"{hoy}%"))
     existing = c.fetchone()
     if existing:
